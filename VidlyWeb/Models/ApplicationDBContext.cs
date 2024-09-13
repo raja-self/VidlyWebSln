dd7857df396ae048dcd716b2187505ffa5905fea
RajasekharMaltech <rajasekharchalla.maltech@gmail.com>
2024-09-13 21:52:46 +0530

    Add project files.

---
+++ VidlyWeb/Models/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using VidlyWeb.Models;
+namespace VidlyWeb.Models
+{
+    public class ApplicationDBContext : IdentityDbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+
+        public ApplicationDBContext()
+          : base("DefaultConnection")
+        {
+        }
+    }
+}
